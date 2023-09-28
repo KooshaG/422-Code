d124d6aeda56f4a41abe6e50141f53bb147f2194
KooshaG <75464454+KooshaG@users.noreply.github.com>
2023-09-28 01:03:32 -0400

    oled test

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -1,13 +1,45 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_ADDRESS    0x3C
+#define OLED_SDA    4
+#define OLED_SCL    15
+#define OLED_RST    16
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
 void setup()
 {
-	pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+  for(;;); // Don't proceed, loop forever
+}
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.setCursor(0,0);
+  display.print("Hi :)");
+  display.display();
+
+  pinMode(BUILTIN_LED, OUTPUT);
 }
 
 void loop()
 {
-	digitalWrite(BUILTIN_LED, HIGH);
-  delay(200);
-	digitalWrite(BUILTIN_LED, LOW);
-  delay(200);
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(1000);
 
 }
