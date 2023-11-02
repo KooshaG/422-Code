3a728bf5d60b10bb9815e5362e95980bb820cbb3
KooshaG <75464454+KooshaG@users.noreply.github.com>
2023-11-02 13:10:39 -0400

    part a mostly done

---
+++ Lab3/PartA/PartA.ino
@@ -0,0 +1,192 @@
+#define RED 21
+#define GREEN 13
+#define SERVO 12
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESP32Servo.h>
+
+
+const char* ssid = "SM-G965W8193";
+const char* password = "opcb0593";
+
+Servo servo1;
+int minUs = 1000;
+int maxUs = 2000;
+int pos = 0;
+
+int incorrectAttempts = 0;
+int timeout = 0;
+bool disabled = false;
+
+WebServer server(80);
+
+//Check if header is present and correct
+bool is_authentified() {
+  Serial.println("Enter is_authentified");
+  if (server.hasHeader("Cookie")) {
+    Serial.print("Found cookie: ");
+    String cookie = server.header("Cookie");
+    Serial.println(cookie);
+    if (cookie.indexOf("ESPSESSIONID=1") != -1) {
+      Serial.println("Authentification Successful");
+      return true;
+    }
+  }
+  Serial.println("Authentification Failed");
+  return false;
+}
+
+//login page, also called for disconnect
+void handleLogin() {
+  String msg;
+  if (server.hasHeader("Cookie")) {
+    Serial.print("Found cookie: ");
+    String cookie = server.header("Cookie");
+    Serial.println(cookie);
+  }
+  if (server.hasArg("DISCONNECT")) {
+    Serial.println("Disconnection");
+    server.sendHeader("Location", "/login");
+    server.sendHeader("Cache-Control", "no-cache");
+    server.sendHeader("Set-Cookie", "ESPSESSIONID=0");
+    server.send(301);
+    return;
+  }
+  if (server.hasArg("USERNAME") && server.hasArg("PASSWORD") && incorrectAttempts < 3) {
+    if (server.arg("USERNAME") == "admin" && server.arg("PASSWORD") == "admin") {
+      server.sendHeader("Location", "/");
+      server.sendHeader("Cache-Control", "no-cache");
+      server.sendHeader("Set-Cookie", "ESPSESSIONID=1");
+      server.send(301);
+      Serial.println("Log in Successful");
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(RED, LOW);
+      servo1.write(180);
+      return;
+    }
+    msg = "Wrong username/password! try again.";
+    incorrectAttempts++;
+    if (incorrectAttempts >= 3) {
+      disabled = true; 
+      timeout = 120;
+    }
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, HIGH);
+    Serial.println("Log in Failed");
+  }
+  if (disabled) msg = "Too many incorrect attempts, try again in 2 minutes.";
+  String content = "<html><body><form action='/login' method='POST'>To log in, please use : admin/admin<br>";
+  content += "User:<input type='text' name='USERNAME' placeholder='user name'><br>";
+  content += "Password:<input type='password' name='PASSWORD' placeholder='password'><br>";
+  if (disabled) content += "<input type='submit' name='SUBMIT' value='Submit' disabled></form>" + msg + "<br>";
+  else content += "<input type='submit' name='SUBMIT' value='Submit'></form>" + msg + "<br>";
+  content += "You also can go <a href='/inline'>here</a></body></html>";
+
+  
+  server.send(200, "text/html", content);
+}
+
+void handleLock() {
+  Serial.println("Enter Lock");
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, LOW);
+  servo1.write(0);
+  server.send(200, "text/plain", "Locked");
+}
+
+//root page can be accessed only if authentification is ok
+void handleRoot() {
+  Serial.println("Enter handleRoot");
+  String header;
+  if (!is_authentified()) {
+    server.sendHeader("Location", "/login");
+    server.sendHeader("Cache-Control", "no-cache");
+    server.send(301);
+    return;
+  }
+  String content = "<html><body><H2>hello, you successfully connected to esp8266!</H2><br>";
+  if (server.hasHeader("User-Agent")) {
+    content += "the user agent used is : " + server.header("User-Agent") + "<br><br>";
+  }
+  content += "You can access this page until you <a href=\"/login?DISCONNECT=YES\">disconnect</a></body></html>";
+  server.send(200, "text/html", content);
+}
+
+//no need authentification
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setup(void) {
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  servo1.setPeriodHertz(50);
+  servo1.attach(SERVO, minUs, maxUs);
+
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  server.on("/", handleRoot);
+  server.on("/login", handleLogin);
+  server.on("/lock", handleLock);
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works without need of authentification");
+  });
+
+  server.onNotFound(handleNotFound);
+  //here the list of headers to be recorded
+  const char * headerkeys[] = {"User-Agent", "Cookie"} ;
+  size_t headerkeyssize = sizeof(headerkeys) / sizeof(char*);
+  //ask server to track these headers
+  server.collectHeaders(headerkeys, headerkeyssize);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
+  if (timeout > 0 && disabled) {
+    delay(1000);
+    timeout--;
+    if (timeout % 20 == 0) Serial.printf("%d seconds until login is enabled\n", timeout);
+  }
+  if (timeout == 0 && disabled) {
+    incorrectAttempts = 0;
+    disabled = false;
+  }
+}
+
