c6524b58ce5984dcccf4750e9a815e8c234444ec
KooshaG <75464454+KooshaG@users.noreply.github.com>
2023-11-22 16:06:05 -0500

    fix server

--- Project/website/src/trpc/server.ts
+++ Project/website/src/trpc/server.ts
@@ -3,11 +3,13 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { type AppRouter } from "@/server";
 import { SuperJSON } from 'superjson';
 
+const url = process.env.DEPLOYED ? `https://${process.env.NEXT_PUBLIC_URL}/api/trpc` : `${process.env.NEXT_PUBLIC_URL}/api/trpc`
+
 export const api = createTRPCProxyClient<AppRouter>({
   transformer: SuperJSON,
   links: [
     httpBatchLink({
-      url: `${process.env.NEXTAUTH_URL}/api/trpc`,
+      url: url,
     }),
   ],
 });
