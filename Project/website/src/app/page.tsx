32d4c5f938635990b7a2cec86e78b1529e075005
KooshaG <75464454+KooshaG@users.noreply.github.com>
2023-12-05 00:05:49 -0500

    added menu buttons

--- Project/website/src/app/page.tsx
+++ Project/website/src/app/page.tsx
@@ -2,7 +2,23 @@ import { getServerSession } from "next-auth"
 // import { useSession } from "next-auth/react";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { api } from '@/trpc/server'
+import Link from "next/link";
 // import { api } from '@/trpc/client'
+
+const navLinks = [
+  {
+    nav: "/doorbells",
+    name: "Your Doorbells",
+  }, 
+  {
+    nav: "/add",
+    name: "Add Doorbell",
+  },
+  {
+    nav: "/test",
+    name: "Test",
+  }, 
+];
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -25,12 +41,14 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
       <div>
         <p className="text-5xl">Hello {session ? session.user.name : ""}!</p>
         <p className="text-3xl">Press any of the buttons to start using Any Bell</p>
       </div>
-      TODO: Add buttons that link same as tabs
+      <div className="flex flex-col w-full md:w-96 gap-4">
+        {navLinks.map((navLink) => <Link href={navLink.nav} key={navLink.nav} className="btn btn-secondary">{navLink.name}</Link>)}
+      </div>
     </main>
   )
 }
